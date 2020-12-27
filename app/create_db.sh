f35d97c155488dc717d521f5bfbe418f5c6f1958
care1e55 <sfilms89@gmail.com>
2020-12-27 18:00:56 +0300

    change to docker-compose application

--- app/create_db.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-/bin/sleep 5
-psql postgresql://postgres:example@localhost:5432/postgres -c "CREATE TABLE IF NOT EXISTS TEST(txt VARCHAR, num INT)"
-psql postgresql://postgres:example@localhost:5432/postgres -c "\copy TEST(txt, num) FROM '/usr/local/data/data.csv' DELIMITER ',' CSV"
-psql postgresql://postgres:example@localhost:5432/postgres -c "SELECT * FROM TEST" && echo "Data inserted"
