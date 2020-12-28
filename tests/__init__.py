c258230c62eb9b00bd0c6dbb131eb24f3fe2fb80
care1e55 <sfilms89@gmail.com>
2020-12-28 16:54:49 +0300

    change

--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from .controllers_test import TestControllers
-
--- tests/clean_test_data.sql
+++
@@ -1,9 +0,0 @@
-truncate table auth cascade;
-truncate table students;
-truncate table teachers;
-truncate table user_profile;
-truncate table groups cascade;
-truncate table courses cascade;
-truncate table homeworks cascade;
-truncate table materials;
-truncate table solutions;
