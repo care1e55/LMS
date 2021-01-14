668edc65f15228f5f1794ef065d9cecaac08787e
care1e55 <sfilms89@gmail.com>
2021-01-14 16:59:34 +0300

    add basic token set

--- app/lms/controllers/auth.py
+++ app/lms/controllers/auth.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, g, jsonify
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from lms.model.auth import Auth 
 import logging
 
 from . import Session
 
 auth_api = Blueprint('auth_api', __name__)
+auth_password = HTTPBasicAuth()
+auth_token = HTTPBasicAuth()
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
@@ -55,3 +59,39 @@ def register():
         session.commit()
     session.close()
     return 'OK', 200
+
+
+@auth_password.verify_password
+def verify_password(user_id, password):
+    session = Session()
+    g.user = session.query(Auth).filter_by(user_id=user_id).first()
+    if g.user is None:
+        return False
+    return g.user.verify_password(password)
+
+@auth_token.verify_password
+def verify_auth_token(token, unused):
+    g.user = Auth.verify_auth_token(token)
+    return g.user is not None
+
+@auth_password.error_handler
+def unauthorized():
+    response = jsonify({'status': 401, 'error': 'unauthorized',
+                        'message': 'please authenticate'})
+    response.status_code = 401
+    return response
+
+@auth_token.error_handler
+def unauthorized_token():
+    response = jsonify({'status': 401, 'error': 'unauthorized',
+                        'message': 'please send your authentication token'})
+    response.status_code = 401
+    return response
+
+# authentication token route
+@auth_api.route('/get-auth-token')
+@auth_password.login_required
+def get_auth_token():
+    response = make_response(jsonify({'token': g.user.generate_auth_token()}))
+    response.set_cookie('token', g.user.generate_auth_token())
+    return response
